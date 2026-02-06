5a02390f60f7d0aa1ecc8245b246c7ec557b596c
kevbanza <kevbanza@gmail.com>
2026-02-06 12:14:22 -0400

    Game style

--- src/assets/components/Die.jsx
+++ src/assets/components/Die.jsx
@@ -5,8 +5,14 @@ function Die(props) {
   
   return (
     <>
-        <div className='col-3'>
-          <button className={props.dieBoolean? 'btn btn-success btn-outline-dark mb-2' : 'btn btn-outline-dark btn-outline-dark mb-2' } onClick={() => props.stopChange(props.id)} >{props.value}</button>
+        <div className='col-5-custom mb-2'>
+          <button
+            className={`die ${props.dieBoolean ? 'die-frozen' : ''}`}
+            onClick={() => props.stopChange(props.id)}
+            aria-pressed={props.dieBoolean}
+          >
+            {props.value}
+          </button>
         </div>
     </>
   )
